58667f94133d70ec54b5d23076ce074d22e37e04
Ayush Jain <ayushjain2205@gmail.com>
2023-08-05 00:14:52 +0530

    Setup thirdweb

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThirdwebProvider
+      activeChain="ethereum"
+      clientId="5dbb8f1abc71686e44ab6ff02c6a9a92"
+    >
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  );
 }
