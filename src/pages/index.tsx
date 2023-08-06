8bfb248d55410f479b78f67a24b7bae27dc10052
Ayush Jain <ayushjain2205@gmail.com>
2023-08-06 21:38:43 +0530

    Add Zora SDK support

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,23 @@
 import { Inter } from "next/font/google";
 import { ConnectWallet } from "@thirdweb-dev/react";
+import { useNFT, useNFTMetadata } from "@zoralabs/nft-hooks";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const { data } = useNFT("0xabEFBc9fD2F806065b4f3C237d4b59D9A97Bcac7", "20");
+
   return (
     <div>
       <h1>Agent</h1>
       <ConnectWallet />
+      {data && (
+        <div>
+          <h3>{data.metadata.name}</h3>
+          <p>{data.metadata.description}</p>
+          <p>Owned by: {data.nft.owner.address}</p>
+        </div>
+      )}
     </div>
   );
 }
