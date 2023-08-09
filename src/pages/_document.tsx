b536f5bd9c29955635c97ba62cd47c5d526e5680
Ayush Jain <ayushjain2205@gmail.com>
2023-08-09 22:15:37 +0530

    fix google fonts import

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en" data-theme="normal">
-      <Head />
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css?family=Work Sans"
+          rel="stylesheet"
+        ></link>
+      </Head>
       <body>
         <Main />
         <NextScript />
