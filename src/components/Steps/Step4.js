d30159a628f514c5bf3967e026dea60762d0aa61
Ayush Jain <ayushjain2205@gmail.com>
2023-08-10 16:05:31 +0530

    Update buy NFT in preview

--- src/components/Steps/Step4.js
+++ src/components/Steps/Step4.js
@@ -2,8 +2,120 @@ import React from "react";
 
 const Step4 = () => {
   return (
-    <div className="flex flex-col h-[539px] gap-[24px] items-center justify-center">
-      Step4
+    <div className="flex flex-col w-[878px] cursor-pointer">
+      <div className="flex flex-row w-full h-[52px] bg-[#262626] justify-between text-white text-[24px] px-[16px] items-center rounded-t-[15px]">
+        Buy NFT
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+        >
+          <path
+            d="M1.93918 1.94035C2.22047 1.65915 2.60193 1.50117 2.99968 1.50117C3.39743 1.50117 3.77889 1.65915 4.06018 1.94035L11.9997 9.87985L19.9392 1.94035C20.0775 1.79709 20.2431 1.68282 20.4261 1.6042C20.6091 1.52559 20.8059 1.48421 21.0051 1.48248C21.2042 1.48075 21.4018 1.5187 21.5861 1.59412C21.7705 1.66954 21.9379 1.78092 22.0788 1.92176C22.2196 2.0626 22.331 2.23008 22.4064 2.41442C22.4818 2.59877 22.5198 2.79629 22.5181 2.99546C22.5163 3.19462 22.4749 3.39146 22.3963 3.57446C22.3177 3.75747 22.2034 3.92298 22.0602 4.06135L14.1207 12.0009L22.0602 19.9404C22.3334 20.2233 22.4846 20.6022 22.4812 20.9955C22.4778 21.3888 22.32 21.765 22.0419 22.0431C21.7638 22.3212 21.3876 22.4789 20.9943 22.4824C20.601 22.4858 20.2221 22.3346 19.9392 22.0614L11.9997 14.1219L4.06018 22.0614C3.77728 22.3346 3.39837 22.4858 3.00508 22.4824C2.61178 22.4789 2.23556 22.3212 1.95745 22.0431C1.67934 21.765 1.52159 21.3888 1.51817 20.9955C1.51475 20.6022 1.66594 20.2233 1.93918 19.9404L9.87868 12.0009L1.93918 4.06135C1.65797 3.78006 1.5 3.3986 1.5 3.00085C1.5 2.60311 1.65797 2.22165 1.93918 1.94035Z"
+            fill="white"
+          />
+        </svg>
+      </div>
+
+      <div className="grid grid-cols-3 gap-[7px] h-[452px] overflow-scroll hide-scrollbar p-[16px] bg-[#fff] rounded-b-[15px]">
+        <div className="flex flex-col rounded-[8px] w-[275px] gap-[8px] min-h-[300px] bg-[#f8f8f8] p-[8px]">
+          <img
+            className="w-[259px] h-[224px] rounded-[8px]"
+            src="https://i.seadn.io/gcs/files/fd7fb6f7a510ef9746332776af07713a.jpg?auto=format&dpr=1&w=1000"
+            alt=""
+          />
+          <p className="flex justify-between items-center text-[20px] font-bold">
+            Entropy_Squad_1
+            <img className="h-[24px]" src="/zora.svg" alt="" />
+          </p>
+          <p className="flex justify-between items-center text-[12px]">
+            NFTs 4 grabs⛓👽💣
+            <span className="text-[18px] ">0.01 ETH</span>
+          </p>
+        </div>
+        <div className="flex flex-col rounded-[8px] w-[275px] gap-[8px] min-h-[300px] bg-[#f8f8f8] p-[8px]">
+          <img
+            className="w-[259px] h-[224px] rounded-[8px]"
+            src="https://i.seadn.io/gcs/files/6306325c8d4f2f084610e85f88efe387.jpg?auto=format&dpr=1&w=1000"
+            alt=""
+          />
+          <p className="flex justify-between items-center text-[20px] font-bold">
+            Entropy_Squad_1
+            <img className="h-[24px]" src="/zora.svg" alt="" />
+          </p>
+          <p className="flex justify-between items-center text-[12px]">
+            NFTs 4 grabs⛓👽💣
+            <span className="text-[18px] ">0.01 ETH</span>
+          </p>
+        </div>
+        <div className="flex flex-col rounded-[8px] w-[275px] gap-[8px] min-h-[300px] bg-[#f8f8f8] p-[8px]">
+          <img
+            className="w-[259px] h-[224px] rounded-[8px]"
+            src="https://i.seadn.io/gcs/files/b6d40f582015cdad01984aacfb1ffbe7.jpg?auto=format&dpr=1&w=1000"
+            alt=""
+          />
+          <p className="flex justify-between items-center text-[20px] font-bold">
+            Entropy_Squad_1
+            <img className="h-[24px]" src="/zora.svg" alt="" />
+          </p>
+          <p className="flex justify-between items-center text-[12px]">
+            NFTs 4 grabs⛓👽💣
+            <span className="text-[18px] ">0.01 ETH</span>
+          </p>
+        </div>
+        <div className="flex flex-col rounded-[8px] w-[275px] gap-[8px] min-h-[300px] bg-[#f8f8f8] p-[8px]">
+          <img
+            className="w-[259px] h-[224px] rounded-[8px]"
+            src="https://i.seadn.io/gcs/files/f5945cfbd75e7909c8dbbd3d9a15132c.jpg?auto=format&dpr=1&w=1000"
+            alt=""
+          />
+          <p className="flex justify-between items-center text-[20px] font-bold">
+            Entropy_Squad_1
+            <img className="h-[24px]" src="/zora.svg" alt="" />
+          </p>
+          <p className="flex justify-between items-center text-[12px]">
+            NFTs 4 grabs⛓👽💣
+            <span className="text-[18px] ">0.01 ETH</span>
+          </p>
+        </div>
+        <div className="flex flex-col rounded-[8px] w-[275px] gap-[8px] min-h-[300px] bg-[#f8f8f8] p-[8px]">
+          <img
+            className="w-[259px] h-[224px] rounded-[8px]"
+            src="https://i.seadn.io/gcs/files/125c94c5c1bc58f9797feb723f2cc451.jpg?auto=format&dpr=1&w=1000"
+            alt=""
+          />
+          <p className="flex justify-between items-center text-[20px] font-bold">
+            Entropy_Squad_1
+            <img className="h-[24px]" src="/zora.svg" alt="" />
+          </p>
+          <p className="flex justify-between items-center text-[12px]">
+            NFTs 4 grabs⛓👽💣
+            <span className="text-[18px] ">0.01 ETH</span>
+          </p>
+        </div>
+        <div className="flex flex-col rounded-[8px] w-[275px] gap-[8px] min-h-[300px] bg-[#f8f8f8] p-[8px]">
+          <img
+            className="w-[259px] h-[224px] rounded-[8px]"
+            src="https://i.seadn.io/gcs/files/c8d0dc4e957ed3e554cbe108329475bb.jpg?auto=format&dpr=1&w=1000"
+            alt=""
+          />
+          <p className="flex justify-between items-center text-[20px] font-bold">
+            Entropy_Squad_1
+            <img className="h-[24px]" src="/zora.svg" alt="" />
+          </p>
+          <p className="flex justify-between items-center text-[12px]">
+            NFTs 4 grabs⛓👽💣
+            <span className="text-[18px] ">0.01 ETH</span>
+          </p>
+        </div>
+      </div>
+      <div className="flex flex-row w-full h-[80px] bg-[#d9d9d9] rounded-b-[15px] px-[24px] py-[16px] justify-end">
+        <button className=" w-[138px] h-[48px] rounded-[8px] bg-[#262626] text-white text-[20px] ">
+          BUY
+        </button>
+      </div>
     </div>
   );
 };
--- src/components/Steps/Step5.js
+++ src/components/Steps/Step5.js
@@ -7,7 +7,12 @@ const Step5 = () => {
       <p className="w-[373px] text-[24px] text-center">
         Minting of NFT was successful
       </p>
-      <a className="text-[#1A64BC] underline" href="">
+      <a
+        className="text-[#1A64BC] underline"
+        target="_blank"
+        rel="noreferrer"
+        href="https://explorer.zora.energy/tx/0x3e8db7458dcd6cecfe145090282fa21c4a506b528bf523c5875ae11b3a69c2f7"
+      >
         View on block explorer
       </a>
     </div>
