173080cafd45245c0527c74f4bc1c690b30b7f8c
Ayush Jain <ayushjain2205@gmail.com>
2023-08-10 16:36:10 +0530

    update choose template cards

--- src/components/Sections/ChooseTemplate.js
+++ src/components/Sections/ChooseTemplate.js
@@ -15,24 +15,28 @@ const ChooseTemplate = ({ handleNext }) => {
     <div className="relative mt-[42px] ">
       <div className="grid grid-cols-4 gap-4">
         <TemplateCard
+          title="Generate NFTs"
+          onSelect={handleSelect}
+          isSelected={selectedTitles.includes("Generate NFTs")}
+          index={1}
+        />
+        <TemplateCard
           title="Buy NFT"
           onSelect={handleSelect}
           isSelected={selectedTitles.includes("Buy NFT")}
+          index={2}
         />
         <TemplateCard
           title="Create Smart Contract"
           onSelect={handleSelect}
           isSelected={selectedTitles.includes("Create Smart Contract")}
+          index={3}
         />
         <TemplateCard
-          title="Mint NFT"
+          title="User Manual"
           onSelect={handleSelect}
-          isSelected={selectedTitles.includes("Mint NFT")}
-        />
-        <TemplateCard
-          title="How to Guide"
-          onSelect={handleSelect}
-          isSelected={selectedTitles.includes("How to Guide")}
+          isSelected={selectedTitles.includes("User Manual")}
+          index={4}
         />
       </div>
       <div className="fixed bottom-0 right-0 w-full h-[80px] bg-[#f9f9f9]">
@@ -53,29 +57,29 @@ const ChooseTemplate = ({ handleNext }) => {
   );
 };
 
-const TemplateCard = ({ title, onSelect, isSelected }) => {
+const TemplateCard = ({ title, onSelect, isSelected, index }) => {
   return (
     <div
       className={`flex flex-col w-[389px] cursor-pointer ${
-        isSelected ? "border-2 border-blue-500" : ""
+        isSelected ? "border-4 border-blue-500 rounded-[20px]" : ""
       }`}
       onClick={() => onSelect(title)}
     >
-      <div className="flex flex-row w-full h-[52px] bg-[#699BF7] text-white text-[24px] px-[16px] items-center rounded-t-[15px]">
+      <div className="flex flex-row w-full h-[52px] bg-[#699BF7] font-bold text-white text-[24px] px-[16px] items-center rounded-t-[15px]">
         {title}
       </div>
       <div className="flex flex-col p-[16px] bg-[#f8f8f8] rounded-b-[15px]">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore,
-          dolorem alias.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore,
-          dolorem alias.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore,
-          dolorem alias.
+        <div className="flex flex-row h-[301px] bg-white justify-center items-center ">
+          <img
+            className="h-[150px] w-[150px]"
+            src={`/cats/cat${index}.svg`}
+            alt=""
+          />
+        </div>
+
+        <p className="text-[20px] mt-[33px] mb-[28px]">
+          Template that can help users buy NFTS within 2 clicks. The agent will
+          also suggest and scout NFTs for the user.
         </p>
       </div>
     </div>
