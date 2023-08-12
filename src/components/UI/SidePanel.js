cd5e2b04c2931589c7f8979e558502517d3a50fa
Ayush Jain <ayushjain2205@gmail.com>
2023-08-12 12:55:45 +0530

    update side panel and next prompt to change on exosytem change

--- src/components/UI/SidePanel.js
+++ src/components/UI/SidePanel.js
@@ -6,16 +6,16 @@ const SidePanel = () => {
   console.log(ecosystem);
 
   const colors = {
-    optimism: "bg-gray-custom",
-    base: "bg-[#0052FE]",
-    zora: "bg-[#909090]",
-    mode: "bg-[#DFFE00]",
+    optimism: { border: "[#fff]", bg: "[#FF0420]", text: "white" },
+    base: { border: "[#0052FE]", bg: "[#0052FE]", text: "white" },
+    zora: { border: "[#909090]", bg: "[#909090]", text: "white" },
+    mode: { border: "[#DFFE00]", bg: "[#DFFE00]", text: "black" },
   };
 
   return (
     <div className="flex flex-col gap-[24px]">
       <div
-        className={`${colors[ecosystem]}  h-[180px] w-[558px] rounded-[20px]`}
+        className={`border border-${colors[ecosystem]["border"]}  h-[180px] w-[558px] rounded-[20px]`}
       >
         <div className="flex flex-row pl-[24px] pt-[44px] pr-[32px] justify-between">
           <div className="flex flex-col gap-[9px] ">
@@ -34,7 +34,7 @@ const SidePanel = () => {
         </div>
       </div>
       <div
-        className={`${colors[ecosystem]}  h-[180px] w-[558px] rounded-[20px]`}
+        className={`border border-${colors[ecosystem]["border"]}  h-[180px] w-[558px] rounded-[20px]`}
       >
         <div className="flex flex-row pl-[24px] pt-[44px] pr-[32px] justify-between">
           <div className="flex flex-col gap-[9px] ">
@@ -53,7 +53,7 @@ const SidePanel = () => {
         </div>
       </div>
       <div
-        className={`${colors[ecosystem]}  h-[180px] w-[558px] rounded-[20px]`}
+        className={`border border-${colors[ecosystem]["border"]}   h-[180px] w-[558px] rounded-[20px]`}
       >
         <div className="flex flex-row pl-[24px] pt-[44px] pr-[32px] justify-between">
           <div className="flex flex-col gap-[9px] ">
@@ -74,24 +74,29 @@ const SidePanel = () => {
         </div>
       </div>
       <div
-        className={`${colors[ecosystem]}  h-[180px] w-[558px] rounded-[20px]`}
+        className={`border border-${colors[ecosystem]["border"]}  flex flex-col justify-center  h-[120px] w-[558px] rounded-[20px]`}
       >
-        <div className="flex flex-row pl-[24px] pt-[28px] pr-[32px] justify-between">
+        <div className="flex flex-row pl-[24px]  pr-[32px] ">
+          <p className="text-[32px] text-[#D9D9D9] w-[426px]">
+            Tell me about <span className="capitalize">{ecosystem}</span>
+          </p>
+          <img className="h-[64px] w-[64px]" src="/speech-bubble.svg" alt="" />
+        </div>
+      </div>
+      <div
+        className={` bg-${colors[ecosystem]["bg"]}  h-[60px] w-[558px] rounded-[20px]`}
+      >
+        <div className="flex flex-row px-[24px] py-[4px] justify-between">
           <div className="flex flex-col gap-[9px] ">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="81"
-              height="4"
-              viewBox="0 0 81 4"
-              fill="none"
-            >
-              <path d="M0 2H81" stroke="#D9D9D9" strokeWidth="4" />
-            </svg>
-            <p className="text-[32px] text-[#D9D9D9]">
-              Tell me about Optimisim
+            <p className={`text-[32px] text-${colors[ecosystem]["text"]}`}>
+              Next prompt
             </p>
           </div>
-          <img className="h-[64px] w-[64px]" src="/speech-bubble.svg" alt="" />
+          <img
+            className="h-[48px] w-[48px]"
+            src="/images/question.svg"
+            alt=""
+          />
         </div>
       </div>
     </div>
