0cf05067314dd409850bb3cc721e6c297df9c436
Ayush Jain <ayushjain2205@gmail.com>
2023-08-12 13:45:32 +0530

    Reconfigure buy template

--- src/components/Steps/Step4.js
+++ src/components/Steps/Step4.js
@@ -19,8 +19,8 @@ const Step4 = () => {
         </svg>
       </div>
 
-      <div className="grid grid-cols-3 gap-[7px] h-[452px] overflow-scroll hide-scrollbar p-[16px] bg-[#fff] rounded-b-[15px]">
-        <div className="flex flex-col rounded-[8px] w-[275px] gap-[6px] min-h-[300px] bg-[#f8f8f8] p-[8px]">
+      <div className="grid grid-cols-3 gap-[7px] h-[452px] overflow-scroll hide-scrollbar p-[16px] bg-[#f8f8f8]">
+        <div className="flex flex-col rounded-[8px] w-[275px] gap-[6px] min-h-[300px] bg-[#fff] p-[8px]">
           <img
             className="w-[259px] h-[224px] rounded-[8px]"
             src="https://i.seadn.io/gcs/files/fd7fb6f7a510ef9746332776af07713a.jpg?auto=format&dpr=1&w=1000"
@@ -34,7 +34,7 @@ const Step4 = () => {
             <span className="text-[18px] ">0.01 ETH</span>
           </p>
         </div>
-        <div className="flex flex-col rounded-[8px] w-[275px] gap-[6px] min-h-[300px] bg-[#f8f8f8] p-[8px]">
+        <div className="flex flex-col rounded-[8px] w-[275px] gap-[6px] min-h-[300px] bg-[#fff] p-[8px]">
           <img
             className="w-[259px] h-[224px] rounded-[8px]"
             src="https://i.seadn.io/gcs/files/6306325c8d4f2f084610e85f88efe387.jpg?auto=format&dpr=1&w=1000"
@@ -48,7 +48,7 @@ const Step4 = () => {
             <span className="text-[18px] ">0.01 ETH</span>
           </p>
         </div>
-        <div className="flex flex-col rounded-[8px] w-[275px] gap-[8px] min-h-[300px] bg-[#f8f8f8] p-[8px]">
+        <div className="flex flex-col rounded-[8px] w-[275px] gap-[8px] min-h-[300px] bg-[#fff] p-[8px]">
           <img
             className="w-[259px] h-[224px] rounded-[8px]"
             src="https://i.seadn.io/gcs/files/b6d40f582015cdad01984aacfb1ffbe7.jpg?auto=format&dpr=1&w=1000"
@@ -62,7 +62,7 @@ const Step4 = () => {
             <span className="text-[18px] ">0.01 ETH</span>
           </p>
         </div>
-        <div className="flex flex-col rounded-[8px] w-[275px] gap-[8px] min-h-[300px] bg-[#f8f8f8] p-[8px]">
+        <div className="flex flex-col rounded-[8px] w-[275px] gap-[8px] min-h-[300px] bg-[#fff] p-[8px]">
           <img
             className="w-[259px] h-[224px] rounded-[8px]"
             src="https://i.seadn.io/gcs/files/f5945cfbd75e7909c8dbbd3d9a15132c.jpg?auto=format&dpr=1&w=1000"
@@ -76,7 +76,7 @@ const Step4 = () => {
             <span className="text-[18px] ">0.01 ETH</span>
           </p>
         </div>
-        <div className="flex flex-col rounded-[8px] w-[275px] gap-[8px] min-h-[300px] bg-[#f8f8f8] p-[8px]">
+        <div className="flex flex-col rounded-[8px] w-[275px] gap-[8px] min-h-[300px] bg-[#fff] p-[8px]">
           <img
             className="w-[259px] h-[224px] rounded-[8px]"
             src="https://i.seadn.io/gcs/files/125c94c5c1bc58f9797feb723f2cc451.jpg?auto=format&dpr=1&w=1000"
@@ -90,7 +90,7 @@ const Step4 = () => {
             <span className="text-[18px] ">0.01 ETH</span>
           </p>
         </div>
-        <div className="flex flex-col rounded-[8px] w-[275px] gap-[8px] min-h-[300px] bg-[#f8f8f8] p-[8px]">
+        <div className="flex flex-col rounded-[8px] w-[275px] gap-[8px] min-h-[300px] bg-[#fff] p-[8px]">
           <img
             className="w-[259px] h-[224px] rounded-[8px]"
             src="https://i.seadn.io/gcs/files/c8d0dc4e957ed3e554cbe108329475bb.jpg?auto=format&dpr=1&w=1000"
@@ -105,7 +105,8 @@ const Step4 = () => {
           </p>
         </div>
       </div>
-      <div className="flex flex-row w-full h-[80px] bg-[#d9d9d9] rounded-b-[15px] px-[24px] py-[16px] justify-end">
+      <div className="flex flex-row w-full h-[80px] bg-[#f8f8f8] rounded-b-[15px] px-[24px] py-[16px] items-center justify-between">
+        <img className="h-[36px] w-[36px]" src="/zora.svg" alt="" />
         <button className=" w-[138px] h-[48px] rounded-[8px] bg-[#262626] text-white text-[20px] ">
           BUY
         </button>
