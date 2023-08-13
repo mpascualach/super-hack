f37323837d92753007b4bfa2f4caa29e2abbac22
Ayush Jain <ayushjain2205@gmail.com>
2023-08-13 15:35:04 +0530

    Update pay page

--- src/components/Sections/Pay.js
+++ src/components/Sections/Pay.js
@@ -1,12 +1,61 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Step4 from "../Steps/Step4";
 import { useSigner } from "@thirdweb-dev/react";
+
+import {
+  EAS,
+  Offchain,
+  SchemaEncoder,
+  SchemaRegistry,
+} from "@ethereum-attestation-service/eas-sdk";
+import { ethers } from "ethers";
+
+export const EASContractAddress = "0xC2679fBD37d54388Ce493F1DB75320D236e1815e"; // Sepolia v0.26
+
+// Initialize the sdk with the address of the EAS Schema contract address
+const eas = new EAS(EASContractAddress);
+console.log(eas);
 
 const Pay = () => {
   const signer = useSigner();
 
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     // Assuming eas is a global object or available somehow
+  //     const offchain = await eas.getOffchain();
+  //     const schemaEncoder = new SchemaEncoder(
+  //       "uint256 eventId, uint8 voteIndex"
+  //     );
+  //     const encodedData = schemaEncoder.encodeData([
+  //       { name: "eventId", value: 1, type: "uint256" },
+  //       { name: "voteIndex", value: 1, type: "uint8" },
+  //     ]);
+
+  //     const offchainAttestation = await offchain.signOffchainAttestation(
+  //       {
+  //         recipient: "0xFD50b031E778fAb33DfD2Fc3Ca66a1EeF0652165",
+  //         // Unix timestamp of when attestation expires. (0 for no expiration)
+  //         expirationTime: 0,
+  //         // Unix timestamp of current time
+  //         time: 1671219636,
+  //         revocable: true, // Be aware that if your schema is not revocable, this MUST be false
+  //         version: 1,
+  //         nonce: 0,
+  //         schema:
+  //           "0xb16fa048b0d597f5a821747eba64efa4762ee5143e9a80600d0005386edfc995",
+  //         refUID:
+  //           "0x0000000000000000000000000000000000000000000000000000000000000000",
+  //         data: encodedData,
+  //       },
+  //       signer
+  //     );
+  //   };
+
+  //   fetchData();
+  // }, [signer]);
+
   const signMessage = async () => {
-    const message = "Hello World";
+    const message = "Attestation request for template creation";
     const signature = await signer.signMessage(message);
     console.log(signature);
   };
